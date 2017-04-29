9e1e7229266082cb73ffec66758299fb03e8c6b9
Thomas <thomas.christof@outlook.com>
2017-04-29 14:16:57 +0200

    Remove test projects

--- TestServerClientPackets/ExamplePacketsOne/Containers/DatabaseResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestServerClientPackets.ExamplePacketsOne.Containers
-{
-    public enum DatabaseResult : int
-    {
-        Success = 0,
-        Exception = 1,
-        Error = 2
-    }
-}
